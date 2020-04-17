d35bec389820a53c42f54bbad13803ae0b62f503
leoxshn <leoxshn@gmail.com>
2020-04-17 11:29:04 +0200

    improved the File.forEachLine function

--- src/linuxMain/kotlin/posidon/mangoTK/io/File.kt
+++ src/linuxMain/kotlin/posidon/mangoTK/io/File.kt
@@ -28,22 +28,25 @@ actual inline class File actual constructor(val path: String) {
     actual fun forEachLine(block: (line: String) -> Unit) {
         val err = nativeHeap.alloc<CPointerVar<GError>>().ptr
         val channel = g_io_channel_new_file(path, "r+", err)
-        val strPtr = nativeHeap.alloc<CPointerVar<ByteVarOf<Byte>>>()
-        val lenPtr = nativeHeap.alloc<ULongVar>()
-        val termPtr = nativeHeap.alloc<ULongVar>()
-        while (true) {
-            g_io_channel_read_line(channel, strPtr.ptr, lenPtr.ptr, null, err)
-            val line = strPtr.value
-            val char = line?.get(0)?.toChar()
-            if (char != '\n' && char != null) {
-                val l = line.toKString()
-                block(l.removeRange(l.lastIndex, l.length))
-            } else break
+        val charPtr = nativeHeap.alloc<UIntVar>()
+        val line = StringBuilder()
+        loop@ while (true) {
+            val status = g_io_channel_read_unichar(channel, charPtr.ptr, err)
+            when (val char = charPtr.value.toInt().toChar()) {
+                '\n' -> {
+                    block(line.toString())
+                    line.clear()
+                }
+                '\uffff' -> {
+                    if (status == GIOStatus.G_IO_STATUS_EOF) {
+                        block(line.toString())
+                        line.clear()
+                        break@loop
+                    } else line.append(char)
+                }
+                else -> line.append(char)
+            }
         }
-        nativeHeap.free(termPtr)
-        nativeHeap.free(lenPtr)
-        nativeHeap.free(strPtr)
-        val error = err.pointed.value?.pointed
-        if (error != null) throw Error(error.message!!.toKString())
+        nativeHeap.free(charPtr)
     }
 }
